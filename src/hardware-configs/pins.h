287e7425c629b02463dcc07151fb85591fa94081
Ranger921 <gejohn921@gmail.com>
2026-01-12 17:41:03 -0500

    Added code for BNO08x IMU

--- src/hardware-configs/pins.h
+++ src/hardware-configs/pins.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <Arduino.h>
 
-static constexpr uint8_t JOY_X_PIN = A4;
-static constexpr uint8_t JOY_Y_PIN = A5;
+static constexpr uint8_t BNO_CS = 10;
+static constexpr uint8_t BNO_INT = 9;
+static constexpr uint8_t BNO_RST = 5;
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,43 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <Adafruit_BNO08x.h>
+#include <Adafruit_BusIO_Register.h>
+#include <Adafruit_Sensor.h>
 #include "hardware-configs/pins.h"
+
+Adafruit_BNO08x bno(BNO_RST);
+sh2_SensorValue sensorValue;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  while (!Serial) delay(5);
+
+  Serial.println("Starting BNO08x SPI...");
+
+  if (!bno.begin_SPI(BNO_CS, BNO_INT)) {
+    Serial.println("Failed to find BNO08x chip");
+    while (1);
+  }
+
+  Serial.println("BNO08x found!");
+
+  bno.enableReport(SH2_ACCELEROMETER, 10000);
 }
 
 void loop() {
-  Serial.print("{"); // Start marker
+  if (bno.getSensorEvent(&sensorValue)) {
+    if (sensorValue.sensorId == SH2_ACCELEROMETER) {
+      Serial.print("{"); // Start marker
 
-  float joy_x = 1023 - analogRead(JOY_X_PIN);
-  float joy_y = analogRead(JOY_Y_PIN);
-
-  Serial.print(joy_x);
-  Serial.print(",");
-  Serial.print(joy_y);
-  Serial.println("}"); // End marker
+      Serial.print(sensorValue.un.accelerometer.x);
+      Serial.print(",");
+      Serial.print(sensorValue.un.accelerometer.y);
+      Serial.print(",");
+      Serial.print(sensorValue.un.accelerometer.z);
+      Serial.println("}"); // End marker
+    }
+  }
 
   delay(50);
 }
